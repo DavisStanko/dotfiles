7628b900a18133bf0c12a93f823ce71e84f917f5
Davis Stanko <davis@davisstanko.com>
2026-06-14 21:18:00 -0500

    refactor: migrate lsp-zero configuration to v4 and update .commonrc with backup excludes and environment overrides

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,24 +1,31 @@
-local lsp = require('lsp-zero').preset({})
+local lsp_zero = require('lsp-zero')
 
-lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
-end)
+-- lsp_attach runs every time an LSP connects to a buffer
+local lsp_attach = function(client, bufnr)
+  -- This provides the default keymaps (gd, K, etc.)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end
 
-lsp.setup()
+lsp_zero.extend_lspconfig({
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  lsp_attach = lsp_attach,
+  float_border = 'rounded',
+})
 
--- You need to setup `cmp` after lsp-zero
+-- Now setup your servers manually or via mason-lspconfig
+-- Example: require('lspconfig').pyright.setup({})
+
+-- Setup Autocomplete (cmp)
 local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Your custom mappings
     ['<C-CR>'] = cmp.mapping.confirm({select = true}),
-
-    -- Select next and previous item
     ['<C-j>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
     ['<C-k>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
-  }
+  }),
 })
